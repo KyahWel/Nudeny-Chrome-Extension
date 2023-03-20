57f35415cdd378315242a5965e0ff47ba0520304
KyahWel <williamcris.hod02@gmail.com>
2023-03-20 11:52:25 +0800

    Added button functionality

--- contentScript.js
+++ contentScript.js
@@ -21,14 +21,15 @@
         let firstString = url.split(':'); // Backlog: check if the image link is using base64 encoding
         let filename = createFilename(10);
         filename = filename.concat('.jpg');
-          chrome.runtime.sendMessage({msg: 'image', index: i, filename:filename, url:images[i].src}, function({data,index}){
-            // Backlog: Add image size checker condition
-            console.log(data.predictions[0].class);
-              if(data.predictions[0].class === "nude"){
-                console.log("is nude");
-                images[index].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
-              }
-          });
+        chrome.runtime.sendMessage({msg: "image"});
+          // chrome.runtime.sendMessage({msg: 'image', index: i, filename:filename, url:images[i].src}, function({data,index}){
+          //   // Backlog: Add image size checker condition
+          //   console.log(data.predictions[0].class);
+          //     if(data.predictions[0].class === "nude"){
+          //       console.log("is nude");
+          //       images[index].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
+          //     }
+          // });
           processedImages.push(images[i]);
       }
     }
--- popup.js
+++ popup.js
@@ -1,17 +1,20 @@
-let isOn = true;
-chrome.runtime.sendMessage({isOn: true});
-
+document.addEventListener('DOMContentLoaded', function() {
+var isOn = false;
+chrome.runtime.sendMessage({msg: "btnstatus", status: "off"});
+    
 const mainButton = document.getElementById("main");
 const images = document.querySelectorAll("img");
 mainButton.addEventListener("click", function() {
+    chrome.tabs.reload();
     if(isOn) {
         mainButton.style.color = "red";
         isOn = false;
-        chrome.runtime.sendMessage({isOn: false});
+        chrome.runtime.sendMessage({msg: "btnstatus", status: "off"});
     }
     else{
-        chrome.runtime.sendMessage({isOn: true});
+        chrome.runtime.sendMessage({msg: "btnstatus", status: "on"});
         mainButton.style.color = "green";
         isOn = true;
     }
+});
 });
